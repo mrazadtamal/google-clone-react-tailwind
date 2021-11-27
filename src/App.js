0ebb4380d78c8ff4abcf5bdc589f95288cf57122
azadtamal <mrazadt@gmail.com>
2021-11-28 00:39:36 +0600

    nav

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className={darkTheme ? "dark" : ""}>
       <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
-        <Nav />
+        <Nav setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
         <Routes />
         <Footer />
       </div>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Nav = () => {
+const Nav = ({ darkTheme }) => {
   return (
-    <div>
-      <h1>nav</h1>
+    <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
+      <div className="flex justify-between items-center space-x-5 w-screen ">
+        <Link to="/">
+          <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-50 dark:text-gray-900">
+            Google search logo
+          </p>
+        </Link>
+        <button
+          type="button"
+          className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg"
+        >
+          {darkTheme ? "Logo Light" : "logo Dark"}
+        </button>
+      </div>
     </div>
   );
 };
