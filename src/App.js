defc9a046313040c30e875eb8821129008c6eb17
azadtamal <mrazadt@gmail.com>
2021-11-28 23:44:07 +0600

    dark

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { useState } from "react";
-import Footer from "./components/Footer/Footer";
+import React, { useState } from "react";
 import Nav from "./components/Nav/Nav";
 import Routes from "./components/Routes/Routes";
+import Footer from "./components/Footer/Footer";
 
-function App() {
-  const { darkTheme, setDarkTheme } = useState(false);
+const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
 
   return (
     <div className={darkTheme ? "dark" : ""}>
-      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+      <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
         <Nav setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
         <Routes />
         <Footer />
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,27 +1,25 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
-const Nav = ({ darkTheme }) => {
+import React from "react";
+
+const Nav = ({ setDarkTheme, darkTheme }) => {
   return (
     <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
       <div className="flex justify-between items-center space-x-5 w-screen ">
         <Link to="/">
-          <p className="text-2xl flex items-center bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-50 dark:text-gray-900">
-            GOOGLE
-            <img
-              className="w-8 p-1 mx-3 text-green-900`"
-              src="https://res.cloudinary.com/abidazad/image/upload/v1638116084/search_lcstsl.png"
-              alt=""
-            />
+          <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-50 dark:text-gray-900">
+            Goggl 🔎
           </p>
         </Link>
         <button
           type="button"
+          onClick={() => setDarkTheme(!darkTheme)}
           className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg"
         >
-          {darkTheme ? "Logo Light" : "logo Dark"}
+          {darkTheme ? "💡 Light" : "🌙 Dark"}
         </button>
       </div>
+      {/* <Search /> */}
     </div>
   );
 };
